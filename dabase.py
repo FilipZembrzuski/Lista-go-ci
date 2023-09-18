2d56fffecd1cc8d772eee23e8a2223a2cdf2ea77
Brzoza <fzembrzuski@gamil.com>
2023-09-18 23:10:53 +0200

    poprawki w działaniu pliku dabase.py

--- dabase.py
+++ dabase.py
@@ -6,11 +6,11 @@ db_Path = "lista_gości.db"
 if os.path.exists(db_Path) != True:
     print("tworzę nową baze")
     open(db_Path, "x")
-    conn = sql.connect("db_Path")
+    conn = sql.connect(db_Path)
     conn.execute("CREATE TABLE `grupy` (`id` INT NOT NULL, `name` TEXT, PRIMARY KEY (`id`))")
     conn.execute("CREATE TABLE `goście` (`id` INT NOT NULL, `name` TEXT NOT NULL, `grupa` INT NOT NULL, `pod_grupa` INT NOT NULL, PRIMARY KEY (`id`))")
 else:
-    conn = sql.connect("db_Path")
+    conn = sql.connect(db_Path)
 
 def add_group(name):
     text = f"INSERT INTO `grupy`(`name`) VALUES ('{name}')"
