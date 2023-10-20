b1f2fd731115910dec2d02871a4b7663813ce5e3
Brzoza <fzembrzuski@gamil.com>
2023-10-20 18:47:34 +0200

    Dodano możliwość dodawania nowych list (start.py, add_list.html, style2.css) zmiana nazw części zmiennych i funkcji z motywu "gość" na "element" Dodano możliwość usuwania list

--- start.py
+++ start.py
@@ -10,10 +10,10 @@ def add_group(name, color):
     conn.commit()
     conn.close()
     
-def add_guest(name, group, ugroup):
+def add_element(name, group, ugroup):
     conn = sql.connect("lista_gości.db")
-    addGuest = f"INSERT INTO `guests`(`name`,'group','u_group') VALUES ('{name}','{group}','{ugroup}')"
-    conn.execute(addGuest)
+    addElement = f"INSERT INTO `guests`(`name`,'group','u_group') VALUES ('{name}','{group}','{ugroup}')"
+    conn.execute(addElement)
     conn.commit()
     conn.close()
     
@@ -52,20 +52,20 @@ def select_ugroup():
     conn.close()
     return(ugroups)
 
-def select_guests():
+def select_elements():
     conn = sql.connect("lista_gości.db")
-    selectGuests = "Select id, `name`, `group`, `u_group`, `active` From `guests` Order by `group` asc;"
+    selectElements = "Select id, `name`, `group`, `u_group`, `active` From `guests` Order by `group` asc;"
     selectColors = "Select color From `groups`"
-    cursor = conn.execute(selectGuests)
+    cursor = conn.execute(selectElements)
     cursor2 = conn.execute(selectColors)
     conn.commit()
     
     colors = []
     for c in cursor2:
         colors.append(c[0])
-    guests = []
+    elements = []
     for g in cursor:
-        guest = {
+        element = {
             'id':g[0],
             'name':g[1],
             'group':g[2],
@@ -73,19 +73,19 @@ def select_guests():
             'status':g[4],
             'color':colors[g[2]-1]
         }
-        guests.append(guest)
+        elements.append(element)
         
     conn.close()
-    return(guests)
+    return(elements)
 
-def disctive_guest(id):
+def disctive_element(id):
     conn = sql.connect("lista_gości.db")
-    dezactiveGroup = f"UPDATE `guests` SET active = FALSE WHERE `id` = {id}"
-    conn.execute(dezactiveGroup)
+    dezactiveElement = f"UPDATE `guests` SET active = FALSE WHERE `id` = {id}"
+    conn.execute(dezactiveElement)
     conn.commit()
     conn.close()
     
-def reactive_guest(id):
+def reactive_element(id):
     conn = sql.connect("lista_gości.db")
     dezactiveGroup = f"UPDATE `guests` SET active = TRUE WHERE `id` = {id}"
     conn.execute(dezactiveGroup)
@@ -99,12 +99,10 @@ def del_group(id):
     conn.commit()
     conn.close()
 
-def del_guest(id):
+def del_element(id):
     conn = sql.connect("lista_gości.db")
-    deleteGuest = f"DELETE FROM `guests` WHERE `id` = {id};"
-    deleteUguest = f"DELETE FROM `guests` WHERE `u_group` = {id};"
-    conn.execute(deleteGuest)
-    conn.execute(deleteUguest)
+    deleteList = f"DELETE FROM `lists` WHERE `id` = {id};"
+    conn.execute(deleteList)
     conn.commit()
     conn.close()
 
@@ -115,23 +113,66 @@ def edit_groups(id, color):
     conn.commit()
     conn.close()
     
+def add_list(name):
+    conn = sql.connect("lista_gości.db")
+    addTable = f"CREATE TABLE `{name}` (`id` INTEGER PRIMARY KEY AUTOINCREMENT, `name` TEXT NOT NULL, `group` INT NOT NULL, `u_group` INT NOT NULL, `active` BOOL NOT NULL)"
+    addList = f"INSERT INTO `lists`(`name`) VALUES ('{name}')"
+    conn.execute(addTable)
+    conn.execute(addList)
+    conn.commit()
+    conn.close()
+
+def select_list():
+    conn = sql.connect("lista_gości.db")
+    selectList = f"Select id, name, disable From `lists`"
+    cursor = conn.execute(selectList)
+    conn.commit()
+    
+    lists = []
+    for l in cursor:
+        list = {
+            'id': l[0],
+            'name':l[1],
+            'status':l[2]
+        }
+        lists.append(list)
+        
+    conn.close()
+    return(lists)
+
+def del_list(id, name):
+    conn = sql.connect("lista_gości.db")
+    deleteList = f"DELETE FROM `lists` WHERE `id` = {id};"
+    deleteTable = f"Drop Table {name};"
+    conn.execute(deleteList)
+    conn.execute(deleteTable)
+    conn.commit()
+    conn.close()
+
+def edit_list(id, name):
+    conn = sql.connect("lista_gości.db")
+    updateList = f"UPDATE `lists` SET name='{name}' WHERE `id` = {id}"
+    conn.execute(updateList)
+    conn.commit()
+    conn.close()
+    
 @app.route("/home")
 @app.route("/")
 @app.route("/show_list")
-def guests_list():
-    guests = select_guests()
-    return render_template("list.html", guests = guests, count = len(guests))
+def show_list():
+    elements = select_elements()
+    return render_template("list.html", el = elements, count = len(elements))
 
-@app.route("/new_guest", methods = ['POST'])
-def new_guest():
+@app.route("/new_element", methods = ['POST'])
+def new_element():
     if request.method == "POST":
-        name = request.form['pname']
-        group = request.form['pgroup']
-        ugroup = request.form['pugroup']
-        add_guest(name, group, ugroup)
-        return redirect("/add_guest", code=302)
+        name = request.form['ename']
+        group = request.form['egroup']
+        ugroup = request.form['eugroup']
+        add_element(name, group, ugroup)
+        return redirect("/add_element", code=302)
     else:
-        return redirect("/add_guest", code=302)
+        return redirect("/add_element", code=302)
     
 @app.route("/new_group", methods = ['POST'])
 def new_group():
@@ -139,37 +180,37 @@ def new_group():
         name = request.form['gname']
         color = request.form['gcolor']
         add_group(name, color)
-    return redirect("/add_guest", code=302)
+    return redirect("/add_element", code=302)
 
-@app.route("/add_guest")
-def add_guests():
+@app.route("/add_element")
+def add_elements():
     groups = select_group()
     ugroups = select_ugroup()
-    return render_template("add_guest.html", groups = groups, ugroups = ugroups)
+    return render_template("add_element.html", groups = groups, ugroups = ugroups)
 
 @app.route("/delete_group", methods = ['POST'])
 def delete_group():
     if request.method == "POST":
         id = request.form['gid']
         del_group(id)
-        return redirect("/add_guest", code=302)
+        return redirect("/add_element", code=302)
     
-@app.route("/delete_guest", methods = ['POST'])
-def delete_guest():
+@app.route("/delete_element", methods = ['POST'])
+def delete_element():
     if request.method == "POST":
-        id = request.form['gid']
+        id = request.form['eid']
         stat = request.form['stat']
         if stat == "1":
-            disctive_guest(id)
+            disctive_element(id)
         else:
-            del_guest(id)
+            del_element(id)
         return redirect("/", code=302)
     
-@app.route("/reload_guest", methods = ['POST'])
-def reload_guest():
+@app.route("/reload_element", methods = ['POST'])
+def reload_element():
     if request.method == "POST":
-        id = request.form['gid']
-        reactive_guest(id)
+        id = request.form['eid']
+        reactive_element(id)
         return redirect("/", code=302)
     
 @app.route("/edit_group", methods = ['POST'])
@@ -178,8 +219,35 @@ def update_group():
         id = request.form['gid']
         color = request.form['ucolor']
         edit_groups(id, color)
-        return redirect("/add_guest", code=302)
+        return redirect("/add_element", code=302)
     
+@app.route("/add_list")
+def select_lists():
+    lists = select_list()
+    return render_template("add_list.html", lists = lists, count = len(lists))
+
+@app.route("/new_list", methods = ['POST'])
+def add_lists():
+    if request.method == "POST":
+        name = request.form['name']
+        add_list(name)
+        return redirect("/add_list", code=302)
+
+@app.route("/drop_list", methods = ['POST'])
+def drop_lists():
+    if request.method == "POST":
+        id = request.form['lid']
+        name = request.form['name']
+        del_list(id, name)
+        return redirect("/add_list", code=302)
+
+@app.route("/edit_list", methods = ['POST'])
+def edit_lists():
+    if request.method == "POST":
+        id = request.form['lid']
+        name = request.form['name']
+        edit_list(id, name)
+        return redirect("/add_list", code=302)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1',port=420,debug=True)
